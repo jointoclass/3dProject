c2efabb594e78f13d0e2d0539d497c48559b011f
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-02-12 17:31:06 +0900

    add

--- Assets/Earth/Scripts/EarthCameraController.cs
+++ Assets/Earth/Scripts/EarthCameraController.cs
@@ -6,7 +6,7 @@ public class EarthCameraController : MonoBehaviour
     public float rotationSpeed = 1f; // 회전 속도 (더 낮게 설정)
     public float zoomSpeed = 2.0f; // 줌 속도
     public float minZoomDistance = 30.0f; // 최소 줌 거리
-    public float maxZoomDistance = 40.0f; // 최대 줌 거리
+    public float maxZoomDistance = 50.0f; // 최대 줌 거리
 
     private float currentZoom = 40.0f; // 초기 줌 거리
     private Vector3 currentRotation; // 현재 회전 각도
